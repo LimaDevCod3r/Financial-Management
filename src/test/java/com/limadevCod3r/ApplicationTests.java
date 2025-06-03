7a606b1cd7763c2dafdfefa8730070e0354abdea
Diego Lima <diegolima20100@gmail.com>
2025-06-03 08:27:10 -0300

    chore: Estrutura inicial de um projeto spring boot

---
+++ src/test/java/com/limadevCod3r/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limadevCod3r;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
