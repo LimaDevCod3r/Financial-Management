7facdd2d1f20ac5982089ea26ae52e80398a38bf
Diego Lima <diegolima20100@gmail.com>
2025-06-03 08:42:52 -0300

    feat: criar entidade Balance com auditoria de criação e atualização
    - Adicionada a entidade Balance com campos: id, description, amount, createdAt e updatedAt
    - Configurada a auditoria automática para data de criação e última atualização usando @CreatedDate e @LastModifiedDate
    - Ativada a auditoria no projeto com @EnableJpaAuditing

---
+++ src/main/java/com/limadevCod3r/balance/model/Balance.java
@@ -0,0 +1,45 @@
+package com.limadevCod3r.balance.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_balance")
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Balance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
+
+    private String id;
+
+    @Column(name = "balance_description", length = 150, nullable = false)
+
+    private String description;
+
+    @Column(name = "balance_amount", precision = 10, scale = 2, nullable = false)
+
+    private BigDecimal amount;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
